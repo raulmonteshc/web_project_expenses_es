0f5fb4128e6f8e2245e93a880377a17f86029f6a
raulmonteshc <raulmonteshc@gmail.com>
2026-05-01 00:29:26 -0600

    adding functions for stats

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,5 +1,6 @@
 let budgetValue = 0;
 let totalExpensesValue = 0;
+let balanceColor = "green";
 
 const expenseEntries = [
   ["groceries", 33],
@@ -27,4 +28,59 @@ function calculateAverageExpense() {
 
 function calculateBalance() {
   return budgetValue - totalExpensesValue;
+}
+
+function updateBalanceColor() {
+  const balance = calculateBalance();
+
+  if (balance < 0) {
+    balanceColor = "red";
+  } else if (balance < budgetValue * 0.25) {
+    balanceColor = "orange";
+  } else {
+    balanceColor = "green";
+  }
+}
+
+function calculateCategoryExpenses(category) {
+  let categoryTotal = 0;
+
+  for (let i = 0; i < expenseEntries.length; i++) {
+    if (expenseEntries[i][0] === category) {
+      categoryTotal += expenseEntries[i][1];
+    }
+  }
+
+  return categoryTotal;
+}
+
+function calculateLargestCategory() {
+  const categories = [
+    "groceries",
+    "restaurants",
+    "transport",
+    "home",
+    "subscriptions",
+  ];
+
+  const categoriesData = [];
+
+  for (let i = 0; i < categories.length; i++) {
+    const categoryName = categories[i];
+    const categoryTotal = calculateCategoryExpenses(categoryName);
+
+    categoriesData.push([categoryName, categoryTotal]);
+  }
+
+  let largestCategory = categoriesData[0][0];
+  let largestCategoryValue = categoriesData[0][1];
+
+  for (let i = 1; i < categoriesData.length; i++) {
+    if (categoriesData[i][1] > largestCategoryValue) {
+      largestCategory = categoriesData[i][0];
+      largestCategoryValue = categoriesData[i][1];
+    }
+  }
+
+  return largestCategory;
 }
